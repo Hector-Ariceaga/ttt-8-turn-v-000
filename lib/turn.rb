74121de9b3db909007d86befa69ec1c6bf0c6694
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-04-09 22:47:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,4 +10,5 @@ def valid_move? (board, index)
   if index.between?(0,8) && !(position_taken?)
 end
 
-position_taken
+def position_taken (board, index)
+  if board[index] = " " || board[index] = "" 
